b936a0dc69a16520b13754ce56a3b53b3ed014ab
Frankie <emcredmond@smith.edu>
2024-09-16 19:55:12 -0400

    Update price/update OS

--- computer.py
+++ computer.py
@@ -8,7 +8,7 @@ class Computer:
     operating_system: str
     year_made: int
     price: int
-    item_iD: int
+    #item_id: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
 
@@ -20,18 +20,28 @@ class Computer:
         self.operating_system= operating_system
         self.year_made= year_made
         self.price= price
-    def Update_price (self.price, new_price)
+    def update_price(self, new_price):
+        self.price=new_price
+        print ("updated price", self.price)
+    def update_OS(self, new_operatingsystem):
+        self.operating_system=new_operatingsystem
+        print ("updated OS", self.operating_system)
 
-
-
+        
+    
 
     # What methods will you need?
+   # main
+    #update price
+   # update OS
+
 def main():
     
     # First, let's make a computer
-    computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+    computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500,)
     print (computer.description, computer.processor_type, computer.hard_drive_capacity, computer.memory, computer.operating_system, computer.year_made, computer.price)
-
+    computer.update_price(50)
+    computer.update_OS("small sur")
 # only call main if running directly
 if __name__ == "__main__":
     main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,11 +1,37 @@
+from typing import Dict, Optional
+from computer import Computer
 class ResaleShop:
 
     # What attributes will it need?
-    inventory
-    
+    inventory : dict[int, dict] = {}
+    item_id: int
+
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self, ):
-        pass # You'll remove this when you fill out your constructor
-
+    def __init__(self, inventory, ):
+        self.inventory= inventory
+        self.item_id=0
     # What methods will you need?
+        #buy computer
+        #selling a computer
+        #refurbishing a computer
+    def buy(self, computer):
+        self.inventory.append (computer)
+        self.item_id+= 1
+        print (self.item_id)
+    def sell(self, item_id ):
+        self.inventory.remove (item_id)
+        print ("Item sold", self.item_id)
+
+def main():
+    computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500,)
+    computer_2 = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500,)
+    print (computer.description, computer.processor_type, computer.hard_drive_capacity, computer.memory, computer.operating_system, computer.year_made, computer.price)
+    computer.update_price(50)
+    resaleshop= ResaleShop ([])
+    resaleshop.buy(computer)
+    resaleshop.buy(computer_2)
+
+if __name__ == "__main__":
+    main()
