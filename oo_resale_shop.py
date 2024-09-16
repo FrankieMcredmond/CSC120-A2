f026fd36d271c4157446db3ac2bb217ff43e4d5b
Frankie <emcredmond@smith.edu>
2024-09-16 16:04:41 -0400

    getting started

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,11 @@
 class ResaleShop:
 
     # What attributes will it need?
-
+    inventory
+    
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
+    def __init__(self, ):
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
